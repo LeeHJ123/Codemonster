dc3e2bb60c06d1627b493827bdce6c90b1940740
choiseokman <choisw6148@naver.com>
2021-07-27 03:42:06 +0900

    de

--- project/app/src/main/java/com/example/myapplication/HomeActivity.java
+++ project/app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button button=findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
