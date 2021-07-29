169cb825ee72e092ed96155e2b61fda6df7dc65b
jerruy7220 <rnqhstlr2297@naver.com>
2021-07-30 00:56:12 +0900

    dad

--- project/app/src/main/java/com/example/myapplication/LoginRequest.java
+++ project/app/src/main/java/com/example/myapplication/LoginRequest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class LoginRequest extends StringRequest {
 
     //서버 설정 (php파일 연동)
-    final static private String URL = "http://220.122.46.167:8000/Login.php";
+    final static private String URL = "http:// 192.168.35.100/Login.php";
     private Map<String, String> map;
 
     public LoginRequest(String userID, String userPassword, Response.Listener<String> listener) {
