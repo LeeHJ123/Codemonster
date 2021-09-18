1c5d119f5728157e11846b97e96aba44f6c08dd3
jerruy7220 <rnqhstlr2297@naver.com>
2021-09-18 16:18:50 +0900

    hello

--- project/app/src/main/java/com/example/myapplication/GoRequest.java
+++ project/app/src/main/java/com/example/myapplication/GoRequest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class GoRequest extends StringRequest {
     //서버 설정 (php파일 연동)
-    final static private String URL = "http://192.168.35.100/go.php"; //220.122.46.167:8000
+    final static private String URL = "http://192.168.35.74/go.php"; //220.122.46.167:8000
     private Map<String, String> map;
 
     public GoRequest(String userID ,Response.Listener<String> listener){
